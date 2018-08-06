edef365431d99eadf2866c0eac3ab81f37c4fc97
Kamil Purzynski <kamil.purzynski@gmail.com>
2018-08-06 19:44:59 +0200

    Added HistoryByMinute model and created migration file.

---
+++ crypto_net/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import HistoryByMinute
+
+
+admin.site.register(HistoryByMinute)
---
+++ crypto_net/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class HistoryByMinute(models.Model):
+    time = models.IntegerField()
+    high = models.IntegerField()
+    low = models.IntegerField()
+    open = models.IntegerField()
+    close = models.IntegerField()
+    volumefrom = models.IntegerField()
+    volumeto = models.IntegerField()
+    crypto = models.CharField(max_length=10)
+    curr = models.CharField(max_length=10)
