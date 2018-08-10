4ec5896295b8b724cbe691bb60bfcdfdfc3e2082
Kamil Purzynski <kamil.purzynski@gmail.com>
2018-08-10 14:57:54 +0200

    Created model from json

--- crypto_net/forms.py
+++ crypto_net/forms.py
@@ -11,19 +11,10 @@ class HistoryByMinuteForm(forms.Form):
         params = {'fsym': 'ETH', 'tsym': 'PLN', 'limit': 100}
         response = requests.get("https://min-api.cryptocompare.com/data/histominute", params)
         result = json.loads(response.text)
-        x = json.loads(response.text, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
-        HistoryByMinute.objects.create(x.Data[0], crypto='ETH', curr='PLN')
-        #histo = HistoryByMinute(
-        #    time=x.Data[0].time,
-        #    high=x.Data[0].high,
-        #    low=x.Data[0].low,
-        #    open=x.Data[0].open,
-        #    close=x.Data[0].close,
-        #    volumefrom=x.Data[0].volumefrom,
-        #    volumeto=x.Data[0].volumeto,
-        #    crypto='ETH',
-        #    curr='PLN')
-
+        one_history = result['Data'][0]
+        one_history['crypto'] = 'ETH'
+        one_history['curr'] = 'PLN'
+        histo = HistoryByMinute.objects.create(**one_history)
         histo.save()
 
         return response
