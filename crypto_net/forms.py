e121144a530e9ee3d586106683f2b508d0d7a200
Kamil Purzynski <kamil.purzynski@gmail.com>
2018-08-11 21:30:48 +0200

    Started to deal with getting crypto history by timestamp

--- crypto_net/forms.py
+++ crypto_net/forms.py
@@ -1,16 +1,25 @@
-from django.forms import forms
-from collections import namedtuple
-from .models import HistoryByMinute
-import requests
 import json
+import time
+
+import requests
+from django.forms import forms
+
+from .models import HistoryByMinute
 
 
 class HistoryByMinuteForm(forms.Form):
     @staticmethod
     def sync_history():
-        params = {'fsym': 'ETH', 'tsym': 'PLN', 'limit': 100}
+        lastHistory = HistoryByMinute.objects.latest('time')
+
+        ts = int(time.time())
+        params = {'fsym': 'ETH', 'tsym': 'PLN', 'limit': 100, 'toTS': ts}
+
         response = requests.get("https://min-api.cryptocompare.com/data/histominute", params)
+
         result = json.loads(response.text)
+
+        # TODO extract to method
         one_history = result['Data'][0]
         one_history['crypto'] = 'ETH'
         one_history['curr'] = 'PLN'
