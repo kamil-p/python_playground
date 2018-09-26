a64feedf29f25410a6f3e7cca344c7e8fde71d6d
Kamil Purzynski <kamil.purzynski@gmail.com>
2018-09-26 18:41:37 +0200

    Playing with tf

--- neural_network/forms.py
+++ neural_network/forms.py
@@ -70,32 +70,42 @@ class NeuralNetworkForm(forms.Form):
     @staticmethod
     def tf_estimator():
         # 1 Million Points
-        x_data = np.linspace(0.0,10.0,1000000)
+        x_data = np.linspace(0.0, 10.0, 1000000)
 
         noise = np.random.randn(len(x_data))
 
         # y = mx + b + noise_levels
         b = 5
 
-        y_true =  (0.5 * x_data ) + 5 + noise
+        y_true = (0.5 * x_data) + 5 + noise
 
-        my_data = pd.concat([pd.DataFrame(data=x_data,columns=['X Data']),pd.DataFrame(data=y_true,columns=['Y'])],axis=1)
+        my_data = pd.concat([pd.DataFrame(data=x_data, columns=['X Data']), pd.DataFrame(data=y_true, columns=['Y'])],
+                            axis=1)
 
-        my_data.sample(n=250).plot(kind='scatter',x='X Data',y='Y')
+        my_data.sample(n=250).plot(kind='scatter', x='X Data', y='Y')
 
         batch_size = 8
 
         m = tf.Variable(0.5)
         b = tf.Variable(1.0)
 
-        xph = tf.placeholder(tf.float32,[batch_size])
-        yph = tf.placeholder(tf.float32,[batch_size])
+        xph = tf.placeholder(tf.float32, [batch_size])
+        yph = tf.placeholder(tf.float32, [batch_size])
 
-        y_model = m*xph + b
+        y_model = m * xph + b
 
-        error = tf.reduce_sum(tf.square(yph-y_model))
+        error = tf.reduce_sum(tf.square(yph - y_model))
 
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
         train = optimizer.minimize(error)
 
         init = tf.global_variables_initializer()
+
+        with tf.Session() as sess:
+            sess.run(init)
+            batches = 1000
+            for i in range(batches):
+                rand_ind = np.random.randint(len(x_data), size=batch_size)
+                feed = {xph: x_data[rand_ind], yph: y_true[rand_ind]}
+                sess.run(train, feed_dict=feed)
+            model_m, model_b = sess.run([m, b])
